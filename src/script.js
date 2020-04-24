46b3f3b7d81ed528864899263cc142895d2687b0
Steveen <steveen.echeverri@gmail.com>
2020-04-24 18:25:40 -0500

    New intance pack in test

--- src/script.js
+++ src/script.js
@@ -1,33 +1,45 @@
-'use strict';
-
 // <code class="JavaScript"></code>
 
 
-console.log('alex');
+console.log('alex')
 var myCode = document.createElement('code');
 
 var textnode = document.createTextNode('\n\nvar nurseOlynyk = ...');
 
 myCode.appendChild(textnode);
 
-var codeIds = ['es5-one-code', 'es5-two-code', 'es5-three-code', 'es5-four-code'];
-var percentIds = ['es5-one-percent', 'es5-two-percent', 'es5-three-percent', 'es5-four-percent'];
 
-var codeBlocks = ['\n\nvar nurseOlynyk = ...', '\n\nvar nurseDurant = ...', '\n\nvar myString = \'str\' + x', 'var HospitalEmployee = \n  function () {\n    function \n    HospitalEmployee(name) {\n...'];
+
+
+var codeIds = ['es5-one-code',
+               'es5-two-code',
+               'es5-three-code',
+               'es5-four-code'];
+var percentIds = ['es5-one-percent',
+                  'es5-two-percent',
+                  'es5-three-percent',
+                  'es5-four-percent'];
+
+var codeBlocks = ['\n\nvar nurseOlynyk = ...',
+                  '\n\nvar nurseDurant = ...',
+                  '\n\nvar myString = \'str\' + x',
+                  'var HospitalEmployee = \n  function () {\n    function \n    HospitalEmployee(name) {\n...'];
 
 var codeIncrement = 0;
 
-codeIds.forEach(function (codeId) {
-  // Add es5 code blocks to right side of web page.
+
+codeIds.forEach(function(codeId) {
+	// Add es5 code blocks to right side of web page.
   var myCode = document.createElement('code');
   var textnode = document.createTextNode(codeBlocks[codeIncrement]);
   myCode.appendChild(textnode);
   document.getElementById(codeId).appendChild(myCode);
 
+
   // Remove empty ES5 compatibility
   var list = document.getElementById(percentIds[codeIncrement]);
   list.removeChild(list.childNodes[1]);
-
+  
   // Add paragraph with new % compatibility
   var myParagraph = document.createElement('p');
   var textnode = document.createTextNode('98% Compatibility');
@@ -36,5 +48,6 @@ codeIds.forEach(function (codeId) {
 
   codeIncrement++;
 });
+
 
 // Add the new paragraph compatibility
